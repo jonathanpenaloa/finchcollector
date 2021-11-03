29500d9f34b0818a2d959e4329ef00a4848f009f
Arizbet Pena <beth@Arizbets-MacBook-Pro.local>
2021-11-02 18:05:36 -0700

    lab three

--- main_app/models.py
+++ main_app/models.py
@@ -22,17 +22,20 @@ class Finch(models.Model):
         return reverse('detail', kwargs={'finch_id': self.id})
 
 class Feeding(models.Model):
-    date = models.DateField()
+    date = models.DateField('feeding date')
     meal = models.CharField(
         max_length=1,
         choices=MEALS,
         default=MEALS[0][0]
     )
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
+
 
     finch = models.ForeignKey(
         Finch,
         on_delete=models.CASCADE
     )
-
-def __str__(self):
-    return f"{self.get_meal_display()} on {self.date}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.db.models import fields
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Finch
+from .forms import FeedingForm 
 
 def home(request):
     return render(request, 'home.html')
@@ -17,7 +18,20 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
 
 # Create your views here.
 
