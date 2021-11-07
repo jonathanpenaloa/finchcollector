74eba68993d2251068977844a81cd556ba782ccb
Arizbet Pena <beth@Arizbets-MacBook-Pro.local>
2021-11-07 14:15:52 -0800

    nav bar working

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('finches/<int:pk>/update/', views.FinchUpdate.as_view(), name='finches_update'),
     path('finches/<int:pk>/delate/', views.FinchDelete.as_view(), name='finches_delete'),
     path('finches/<int:finch_id>/add_feeding/', views.add_feeding, name='add_feeding'),
+    
     path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
     path('finches/<int:finch_id>/dessoc_toy/<int:toy_id>/', views.dissoc_toy, name='dissoc_toy'),
     path('finches/<int:finch_id>/add_photo/', views.add_photo, name='add_photo'),
